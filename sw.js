2cd7b970fb5b2759c71e08e579c554b2afcedb44
zadshogar <cars@stormepic.com>
2026-02-26 10:24:18 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'checkbook6-v1';
+const ASSETS = [
+  '/checkbook6/',
+  '/checkbook6/index.html',
+  '/checkbook6/manifest.json',
+  '/checkbook6/icon-192.png',
+  '/checkbook6/icon-512.png'
+];
+
+// Install: cache core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match('/checkbook6/'));
+    })
+  );
+});
